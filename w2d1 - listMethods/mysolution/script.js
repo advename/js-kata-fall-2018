10e97076cefe725016485235f84cee00c0be04d5
advename <lars@advena.me>
2018-09-04 10:18:26 +0200

    Finished w2d1

---
+++ w2d1 - listMethods/mysolution/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function listMethods(id) {
+  let lis = document.querySelectorAll("#" + id + " li");
+
+  lis.forEach(li => {
+    displayElement(li);
+  });
+}
+
+function listMethodsFor(id) {
+  const lis = document.querySelectorAll("#" + id + " li");
+  for (let i = 0; i < lis.length; i++) {
+    displayElement(lis[i]);
+  }
+}
+
+function displayElement(element) {
+  console.log(element.textContent);
+}
+listMethods("info");
+console.log("+++");
+listMethods("modify");
+console.log("+++");
+listMethods("newarray");
+
+console.log("======================");
+listMethodsFor("info");
+console.log("+++");
+listMethodsFor("modify");
+console.log("+++");
+listMethodsFor("newarray");
+
+// TODO: Create listMethods function
